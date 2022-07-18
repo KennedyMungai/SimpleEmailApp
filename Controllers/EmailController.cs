cc81d29eb6ef347f81d972dba46201819442bbca
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-18 11:41:47 +0300

    To continue from here

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 
 namespace SimpleEmailApp.Controllers;
 
@@ -7,5 +11,19 @@ namespace SimpleEmailApp.Controllers;
 [ApiController]
 public class EmailController : ControllerBase
 {
-    
+    [HttpPost]
+    public IActionResult SendEmail(string body)
+    {
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse("candace82@ethereal.email"));
+        email.To.Add(MailboxAddress.Parse("candace82@ethereal.email"));
+        email.Subject = "Test Email Subject";
+        email.Body = new TextPart(TextFormat.Html)
+        {
+            Text = body
+        };
+
+        using var smtp = new SmtpClient();
+        smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+    }
 }
