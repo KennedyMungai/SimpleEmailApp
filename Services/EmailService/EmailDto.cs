391c635232e039c746742e254bf76fa2082f27ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-18 12:53:34 +0300

    Added an Email Service DTO

---
+++ Services/EmailService/EmailDto.cs
@@ -0,0 +1,8 @@
+namespace SimpleEmailApp.Services.EmailService;
+
+public class EmailDto
+{
+    public string To { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+}
