1302ed582f3350dbdd9c0bb46a40d3f6528dcda5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-18 13:00:14 +0300

    Added code from the Email controller

--- Services/EmailService/EmailService.cs
+++ Services/EmailService/EmailService.cs
@@ -1,6 +1,27 @@
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
 namespace SimpleEmailApp.Services.EmailService;
 
 public class EmailService : IEmailService
 {
-    
+    public void SendEmail(EmailDto request)
+    {
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse("betsy.cummerata1@ethereal.email"));
+        email.To.Add(MailboxAddress.Parse("betsy.cummerata1@ethereal.email"));
+        email.Subject = "Test Email Subject";
+        email.Body = new TextPart(TextFormat.Html)
+        {
+            Text = request.Body
+        };
+
+        using var smtp = new SmtpClient();
+        smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+        smtp.Authenticate("betsy.cummerata1@ethereal.email", "VGczR1PzqRJby7zGJz");
+        smtp.Send(email);
+        smtp.Disconnect(true);
+    }
 }
