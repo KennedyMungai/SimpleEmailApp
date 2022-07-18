a9415bb3c72b296e55e46c2866b73dc02bb66d91
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-18 12:59:23 +0300

    Moved the code to Email Service

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MimeKit;
-using MimeKit.Text;
-using MailKit.Net.Smtp;
-using MailKit.Security;
+
 
 namespace SimpleEmailApp.Controllers;
 
@@ -14,20 +11,7 @@ public class EmailController : ControllerBase
     [HttpPost]
     public IActionResult SendEmail(string body)
     {
-        var email = new MimeMessage();
-        email.From.Add(MailboxAddress.Parse("betsy.cummerata1@ethereal.email"));
-        email.To.Add(MailboxAddress.Parse("betsy.cummerata1@ethereal.email"));
-        email.Subject = "Test Email Subject";
-        email.Body = new TextPart(TextFormat.Html)
-        {
-            Text = body
-        };
-
-        using var smtp = new SmtpClient();
-        smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-        smtp.Authenticate("betsy.cummerata1@ethereal.email", "VGczR1PzqRJby7zGJz");
-        smtp.Send(email);
-        smtp.Disconnect(true);
+        
 
         return Ok();
     }
