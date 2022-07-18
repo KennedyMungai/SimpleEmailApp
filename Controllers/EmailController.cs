a76326ab31cf889985602a78498c7c4d599655e8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-18 10:59:04 +0300

    Added the base code for the ApiController

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -1,1 +1,11 @@
-namesoa
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleEmailApp.Controllers;
+
+[Route("api/{controller}")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    
+}
